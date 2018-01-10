9bb8fc04001e338201407ef9637a980ea8cba4ac
Spencer Bull <spencerbull2554@gmail.com>
2018-01-10 14:19:18 -0600

    hard set target and args
    Signed-off-by: Spencer Bull <spencerbull2554@gmail.com>

--- darkflow/net/yolov2/predict.py
+++ darkflow/net/yolov2/predict.py
@@ -60,7 +60,7 @@ def postprocess(self, net_out, im, save = True):
 
 	if self.FLAGS.UDP:
 		textJSON = json.dumps(resultsForJSON)
-		t = Thread(sendUDPMessage, (textJSON,))
+		t = Thread(target=sendUDPMessage, args=(textJSON,))
 		t.start()
 
 	if not save: return imgcv
